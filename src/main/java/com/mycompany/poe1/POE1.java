dcbdb28d58f1315df06c8fb9fedab4b53f244aed
RC_Student_lab <RC_Student_lab@labG9AEB3.phdvvtwzx4oudioi2mg3lhtmwg.fx.internal.cloudapp.net>
2024-09-30 20:33:26 +0000

    final commit

--- src/main/java/com/mycompany/poe1/POE1.java
+++ src/main/java/com/mycompany/poe1/POE1.java
@@ -13,27 +13,70 @@ public class POE1 {
     
 
     public static void main(String[] args) {
-        //create object for scanner
+        
+        //create object for scanner and call class
         Scanner sc = new Scanner(System.in);
         register registerobj = new register();
         
-        
-        //declaration
+        //declarations
         String firstname;
         String lastname;
         String user;
         String pass;
         
+        //prompt user for their details
         System.out.println("Enter your Firstname: ");
         firstname = sc.next();
         
         System.out.println("Enter your lastname: ");
         lastname = sc.next();
-       
         
+        // Check if the username is valid
+        boolean isUsernameValid = registerobj.checkUserName(sc);
+        String username = "";
+        String password = "";
+
+        if (isUsernameValid) {
+            // Check the password
+            boolean isPasswordValid = registerobj.checkPasswordComplexity(sc);
+            if (isPasswordValid) {
+                
+                // Capture username and password after validation
+                // Retrieve captured username
+                username = registerobj.getUsername();  
+                // Retrieve captured password
+                password = registerobj.getPassword();  
+
+                // Collect first and last names for greeting purposes
+                System.out.println("Enter First Name: ");
+                String firstName = sc.next();
+
+                System.out.println("Enter Last Name: ");
+                String lastName = sc.next();
+
+                // Attempt to log in with the registered credentials
+                boolean isLoginSuccessful = register.loginUser(sc, username, password, firstName, lastName);
+
+                // Output login result
+                if (isLoginSuccessful) {
+                    System.out.println("Login was successful.");
+                } else {
+                    System.out.println("Login failed. Please check your username and password.");
+                }
+            }
+            else {
+                System.out.println("Password validation failed.");
+            }
+        }
+        else {
+            System.out.println("Username validation failed.");
+        }
+        
+        // Close the scanner
+        sc.close();  
     }
     
+}
+     
     
 
-    
-}
--- src/main/java/com/mycompany/poe1/register.java
+++ src/main/java/com/mycompany/poe1/register.java
@@ -25,10 +25,84 @@ public class register {
         if (username.contains("_") && username.length() <= 5) {
             System.out.println("Username successfully captured.");
             return true;
-        } else {
+        } 
+        
+        else {
             System.out.println("Username is not correctly formatted, please ensure that your username contains an underscore and is no more than 5 characters in length.");
             return false;
         }
+        
     }
+    
+    public boolean checkPasswordComplexity( Scanner sc) {
+        System.out.println("Create your password:");
+        password = sc.next();
+
+        boolean hasDigit = false;
+        boolean hasCapital = false;
+        boolean hasSpecial = false;
+        boolean isLong = password.length() >= 8;
+
+        String specialCharacters = "+=-_)(*&^%$#@!";
+
+        // Check password for the required conditions
+        for (int i = 0; i < password.length(); i++) {
+            char result = password.charAt(i);
+            
+            if (Character.isDigit(result)) {
+                hasDigit = true;
+            }
+            
+            if (Character.isUpperCase(result)) {
+                hasCapital = true;
+            }
+            
+            if (specialCharacters.indexOf(result) >= 0) {
+                hasSpecial = true;
+            }
+        }
+
+        if (hasDigit && hasCapital && hasSpecial && isLong) {
+            System.out.println("Password successfully captured.");
+            return true;
+        }
+        
+        else {
+            System.out.println("Password is not formatted correctly. Please ensure that the password contains at least 8 characters, a capital letter, a special character, and a number.");
+            return false;
+        }
+        
+    }
+    
+     public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    
+     public static boolean loginUser(Scanner sc, String storedUsername, String storedPassword, String firstName, String lastName) {
+        System.out.println("Please enter your username for login: ");
+        String enteredUsername = sc.next();
+
+        System.out.println("Please enter your password for login: ");
+        String enteredPassword = sc.next();
+        
+        //display results
+        if (enteredUsername.equals(storedUsername) && enteredPassword.equals(storedPassword)) {
+            // if login is successful
+            System.out.println("Welcome " + firstName + " " + lastName + ", It is great to see you again!");
+            return true;  
+        }
+        
+        else {
+            // if login failed
+            System.out.println("Username or password validation failed.");
+            return false;  
+        }
+    }
+    
+    
     
 }
